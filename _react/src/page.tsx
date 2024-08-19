8c1e8c83b1e55eb112276eacd46d70bf6d4d1f24
arsalanansariofficial <124055716+arsalanansariofficial@users.noreply.github.com>
2024-08-19 20:04:55 +0530

    added react build for contact page

--- _react/src/page.tsx
+++ _react/src/page.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import Home from '@/_components/home';
+import Contact from '@/_components/contact';
 
 import '@/globals.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Home />
+    <Contact />
   </StrictMode>
 );
